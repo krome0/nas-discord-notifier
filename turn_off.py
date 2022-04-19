8d5b72585b80c6f6d89812e65f21a215ab4e1a54
krome0 <sealminc@gmail.com>
2022-04-20 05:10:28 +0900

    Add turn_off.py

---
+++ turn_off.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class Config:
+    webhook_url = '<your webhook url>'
+    nas_name = '<your nas name>'
+    lang = 'kr'
+
+
+data_kr = {
+    'content':f'{Config.nas_name}가 켜졌습니다.',
+    'username':'NAS 알림'
+}
+
+data_en = {
+    'content':f'{Config.nas_name} is turned on',
+    'username':'NAS Notifier'
+}
+
+langdata = {'kr':data_kr, 'en':data_en}
+
+webhook_result = requests.post(Config.webhook_url, json=langdata[Config.lang])
